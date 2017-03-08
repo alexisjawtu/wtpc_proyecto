07ad1d5d8c11e13e288600460ef04577a652147f
Ruben Rosino <rubenr86@gmail.com>
2017-03-08 16:19:32 -0300

    implementacion incompleta del metodo map_positions

--- environment.py
+++ environment.py
@@ -1,9 +1,16 @@
-import numpy as np
-
-Class Environment(Object):
-    """ size is an np.array[2] """
-    def __init__ (self, size):
-        
+class Environment(object):
+    """TODO: documentation"""
+    def __init__ (self, width, height):
+        self.width = width
+        self.height = height
 
     def map_positions (self, lions, gazelles):
-        pass
+        for l in lions:
+            pos = l.position
+            area[0] = pos[0]-l.radius
+            area[1] = pos[0]+l.radius
+            area[2] = pos[1]-l.radius
+            area[3] = pos[1]+l.radius
+            for g in gazelles:
+                if g.position[0] >= area[0] and g.position[0] <= area[1]:
+                    pass
