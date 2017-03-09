c9c2ea09e267149cd489e768966ceea53795ec72
Ruben Rosino <rubenr86@gmail.com>
2017-03-09 18:19:49 -0300

    add out file

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ env = Environment(width,height)
 #Lions and gazelles
 
 num_lion = 10
-num_gazzelle = 30
+num_gazelle = 30
 
 # Lion parameters
  
@@ -23,7 +23,7 @@ probAt = np.random.random()*.5 +.5
 
 # Gazzelle parameters
 
-stepGazzelle = 1
+stepGazelle = 1
 hungerGaz = 0
 
 #Initialize Lions and Gazelle
@@ -34,7 +34,7 @@ for p in range(num_lion):
 	PosL = np.array([np.random.randint(width),np.random.randint(height)])
 	lions.append(Lion(PosL, radLion, probAt))
 
-for p in range(num_gazzelle):
+for p in range(num_gazelle):
 	PosGaz = np.array([np.random.randint(width),np.random.randint(height)])
 	gazelles.append(Gazelle(PosGaz))
 
@@ -49,19 +49,29 @@ f_name = 'log.txt'
 
 t = 0
 T = 10
+
+
 with open (f_name, "w") as out:
     while t < T:
         env.map_positions(lions, gazelles)
-        for lion in lions:
+        
+        lions_out = np.zeros((num_lion,4))
+        gaz_out = []
+        
+        for k, lion in enumerate(lions):
             list_target = lion.get_targets()
             for target in list_target:
                 lion.attack(gazelles[target])
             lion.move_rnd(1, random_direction(), env.get_width(), env.get_height())
-        for gaz in gazelles:
+            lions_out[k] = np.concatenate((np.array([1,k]),lion.get_position())) 
+        for j,gaz in enumerate(gazelles):
             gaz.move_rnd(1, random_direction(), env.get_width(), env.get_height())
+            if gaz.get_alive():
+                gaz_out.append([0,j]+gaz.get_position().tolist())
         
         t = t+1
-        np.savetxt(out,np.array([1,2,3]))
-        out.write("\n\n")
+        np.savetxt(out,lions_out,fmt="%d",delimiter='\t')
+        np.savetxt(out,np.array(gaz_out),fmt="%d",delimiter='\t')
+        out.write("\n")
         
         
